e83e4c8f8dcf5b2a71ea083084b748e8c81fb469
Jonathan ROCHELLI <jonathan.rochelli@crie.i-univ-tlse2.fr>
2019-12-11 11:13:57 +0100

    JGit Java library

---
+++ viewer/src/main/java/changelog/viewer/App.java
@@ -0,0 +1,43 @@
+package changelog.viewer;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.NoHeadException;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.RepositoryBuilder;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+
+
+public class App 
+{
+    public static void main( String[] args ) throws IOException, NoHeadException, GitAPIException
+    {
+
+        File gitDir = new File("C:\\Users\\AdminEtu\\Documents\\github_gitlab_sync\\.git");
+
+        RepositoryBuilder builder = new RepositoryBuilder();
+        Repository repository;
+        repository = builder.setGitDir(gitDir).readEnvironment()
+                .findGitDir().build();
+
+        Git git = new Git(repository);
+        RevWalk walk = new RevWalk(repository);
+        RevCommit commit = null;
+
+        Iterable<RevCommit> logs = git.log().call();
+        Iterator<RevCommit> i = logs.iterator();
+
+        while (i.hasNext()) {
+            commit = walk.parseCommit( i.next() );
+            System.out.println( commit.getFullMessage() );
+
+        }
+        
+        
+    }
+}
